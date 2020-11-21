9b32b65e4fbb1d729ef9f3ec2b6f6073a9d2bafb
Rajashree Joshi <joshi.raj@husky.neu.edu>
2020-11-21 09:44:46 -0500

    util

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const client = require("prom-client");
 
 const db = require("./db/db-config");
 db.sequelize.sync({ force: false }).then(() => {
@@ -13,6 +14,11 @@ app.use(bodyParser.urlencoded({ extended: true, parameterLimit: 50000 }));
 
 const routes = require("./api/routes");
 routes(app);
+
+app.get('/metrics', (req, res) => {
+  res.set('Content-Type', client.register.contentType);
+  res.end(client.register.metrics())
+});
 
 // Publicly accessible to Run health apis
 const port = process.env.PORT || 3000;
